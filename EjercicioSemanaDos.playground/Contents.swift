d91a53512362dfbcf3cbaf97c145149bc03d9b9b
EnriqueHM <enriqhz@gmail.com>
2015-11-09 23:28:52 -0600

    Creación del programa de la semana dos

--- EjercicioSemanaDos.playground/Contents.swift
+++ EjercicioSemanaDos.playground/Contents.swift
@@ -1,5 +1,22 @@
 //: Playground - noun: a place where people can play
 
+//: Created by Juan Enrique Hernandez Martinez
+
+
 import UIKit
 
-var str = "Hello, playground"
+var numeros = 0...100
+
+for n in numeros {
+    if n != 0 {
+        if n >= 30 && n <= 40 {
+            print("\(n)\tViva Swift")
+        } else if n % 5 == 0 {
+            print("\(n)Bingo")
+        } else if n % 2 == 0 {
+            print("\(n)Par")
+        } else if n % 1 == 0 {
+            print("\(n)Impar")
+        }
+    }
+}
